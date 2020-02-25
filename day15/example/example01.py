4c40fe6bec6ef38076e3338a4ee5649e3c187a39
wangyuxiang985 <wangyuxiang985@gmail.com>
2020-02-25 23:08:17 +0800

    =day15 图像和文档处理

---
+++ day15/example/example01.py
@@ -0,0 +1,16 @@
+"""
+图像和办公文档处理
+
+用Pillow操作图像
+Pillow是由从著名的Python图像处理库PIL发展出来的一个分支，
+通过Pillow可以实现图像压缩和图像处理等各种操作。可以使用下面的命令来安装Pillow。
+pip install pillow
+Pillow中最为重要的是Image类，读取和处理图像都要通过这个类来完成
+
+
+处理Excel电子表格
+Python的openpyxl模块让我们可以在Python程序中读取和修改Excel电子表格
+
+处理Word文档
+利用python-docx模块，Pytho 可以创建和修改Word文档
+"""
